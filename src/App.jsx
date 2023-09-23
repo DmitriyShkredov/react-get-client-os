98e87964ba5428bc6ab3ff5eea8accb5c6224eba
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2023-09-23 11:00:52 +0300

    feat: add checkOs function

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,14 @@ function App() {
   const [osName, setOsName] = useState("unknown");
 
   useEffect(() => {
-    setOsName(navigator.userAgent);
+    const checkOs = (agent) => {
+      if (agent.indexOf("Windows") > 0) setOsName("Windows");
+      if (agent.indexOf("Macintosh") > 0) setOsName("Macintosh");
+      if (agent.indexOf("Linux") > 0) setOsName("Linux");
+      if (agent.indexOf("Android") > 0) setOsName("Android");
+      if (agent.indexOf("iPhone") > 0) setOsName("iPhone OS");
+    };
+    checkOs(navigator.userAgent);
   }, []);
 
   return <div className="App">Your OS: {osName}</div>;
